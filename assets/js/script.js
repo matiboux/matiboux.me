58b70348b04adc0521992a35143ac5c0f560031e
Matiboux <matiboux@gmail.com>
2019-09-17 19:41:44 +0200

    Added search form on the timeline page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,26 @@ $(function() {
 	$('[data-toggle="popover"]').popover();
 	$('[data-toggle="tooltip"]').tooltip();
 });
+
+var searchHandler = function(value) {
+	$('.timeline-items .item').hide();
+	
+	var value = (typeof(value) == "string") ? value : $('#timelineSearch [name="searchField"]').val();
+	console.log(value);
+	if(value != "") {
+		if($('#timelineSearch [name="searchTagsOnly"]').prop('checked'))
+			$('.timeline-items .item .tags:contains("' + value + '")').parents('.item').show();
+		else
+			$('.timeline-items .item:contains("' + value + '")').show();
+	}
+	else
+		$('.timeline-items .item').show();
+};
+
+$('#timelineSearch [name="searchField"]').keyup(function() {
+	searchHandler($(this).val());
+});
+$('#timelineSearch [name="searchTagsOnly"]').change(searchHandler);
+$('#timelineSearch [type="reset"]').click(function() {
+	searchHandler("");
+});

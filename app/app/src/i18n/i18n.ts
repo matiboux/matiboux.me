b0fb4391d2c96d1ae7d1d264e8c689239dfd1a47
Matiboux <matiboux@gmail.com>
2024-03-10 23:43:13 +0100

    Fix types to support function overloads type

--- app/app/src/i18n/i18n.ts
+++ app/app/src/i18n/i18n.ts
@@ -42,13 +42,41 @@ function i18n(
 	)
 }
 
-type Tail<T extends any[]> =
-	((...args: T) => any) extends (arg: any, ...tail: infer U) => any
-	? U : never
+// From `ToTuple<Union>`: https://stackoverflow.com/a/70061272
+type UnionToParm<U> = U extends any ? (k: U) => void : never
+type UnionToSect<U> = UnionToParm<U> extends ((k: infer I) => void) ? I : never
+type ExtractParm<F> = F extends { (a: infer A): void } ? A : never
+type SpliceOne<Union> = Exclude<Union, ExtractOne<Union>>
+type ExtractOne<Union> = ExtractParm<UnionToSect<UnionToParm<Union>>>
+type UnionToTuple<Union, Rslt extends any[] = []> =
+    SpliceOne<Union> extends never
+		? [ExtractOne<Union>, ...Rslt]
+		: UnionToTuple<SpliceOne<Union>, [ExtractOne<Union>, ...Rslt]>
 
-function i18nFactory(locale: Parameters<typeof i18n>[0] | undefined)
+type TupleToUnion<Tuple extends any[], Rslt extends any = never> =
+	Tuple extends [infer Head, ...infer Tail]
+		? TupleToUnion<Tail, Head | Rslt>
+		: Rslt
+
+// From https://stackoverflow.com/a/52761156
+type OverloadedParameters<T> =
+	T extends { (...args: infer A1): any; (...args: infer A2): any } ? A1 | A2 :
+	T extends (...args: infer A) => any ? A :
+	any
+
+type ListTail<List extends any[]> = List extends [any, ...infer Tail] ? Tail : never
+
+type ListRecursiveTail<List extends any[], Rslt extends any[] = []> =
+	List extends [infer Head extends any[], ...infer Tail]
+		? ListRecursiveTail<Tail, [...Rslt, ListTail<Head>]>
+		: Rslt
+type UnionRecursiveTail<Union, Rslt extends any[] = []> =
+	TupleToUnion<ListRecursiveTail<UnionToTuple<Union>, Rslt>>
+
+function i18nFactory(locale: OverloadedParameters<typeof i18n>[0] | undefined)
 {
-	return (...args: Tail<Parameters<typeof i18n>>) => i18n(locale ?? defaultLocale, ...args)
+	return (...args: UnionRecursiveTail<OverloadedParameters<typeof i18n>>) =>
+		i18n.apply(i18n, [locale ?? defaultLocale, ...args])
 }
 
 export default i18n

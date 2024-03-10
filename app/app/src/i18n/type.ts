2cf407c258de5de74e2c792bd17a981f92f06b76
Matiboux <matiboux@gmail.com>
2024-03-10 20:36:01 +0100

    Add list of locale codes

--- app/app/src/i18n/type.ts
+++ app/app/src/i18n/type.ts
@@ -4,12 +4,16 @@ import defaultLocaleData from './locales/en'
 
 const defaultLocale = i18nConfig.defaultLocale
 
+const locales = i18nConfig.locales
+	.flatMap(locale => typeof locale === 'string' ? [locale] : locale.codes)
+
 type I18n = Readonly<Record<keyof typeof defaultLocaleData, string>>
 
 type Diff<T, U> = T extends U ? never : T
 
 export {
 	defaultLocale,
+	locales,
 }
 
 export type {

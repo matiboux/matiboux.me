8ff9190da0db05b6a65117d68c6c5fecf1b7fad3
Matiboux <matiboux@gmail.com>
2025-02-24 02:18:00 +0100

    Update layouts & site config

--- app/app/src/site.ts
+++ app/app/src/site.ts
@@ -1,20 +1,32 @@
-import type { I18nKeys } from '~/i18n/type.d.ts'
+import type { Props as BaseProps } from '~/layouts/Base.astro'
 
 export interface Site
 {
-	title?: string
-	description?: string | I18nKeys
-	author?: string
-	keywords?: string[]
-	themeColor?: string
-	favicon?: string
-	lang?: string
+	lang?: BaseProps['lang']
+	title?: BaseProps['title']
+	description?: BaseProps['description']
+	author?: BaseProps['author']
+	keywords?: BaseProps['keywords']
+	generator?: BaseProps['generator']
+	themeColor?: BaseProps['themeColor']
+	viewportScale?: BaseProps['viewportScale']
+	favicon?: BaseProps['favicon']
+	socialTitle?: BaseProps['socialTitle']
+	socialDescription?: BaseProps['socialDescription']
+	socialImage?: BaseProps['socialImage']
+	socialUrl?: BaseProps['socialUrl']
+	socialType?: BaseProps['socialType']
+	socialTwitterCard?: BaseProps['socialTwitterCard']
 }
 
 export const site: Site = {
+	lang: 'en',
 	title: 'Matiboux.me',
 	description: 'My personal website, about me and my projects!',
 	author: 'Matiboux',
 	themeColor: '#202020',
+	viewportScale: 1,
+	socialTitle: true,
+	socialDescription: true,
 	// gtag: 'UA-53429285-6',
 }

af97abba47f25055fefd711d3130087097355391
Matiboux <matiboux@gmail.com>
2025-11-16 16:30:30 +0100

    Update astro config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -6,6 +6,11 @@ import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
+	site: process.env.ASTRO_SITE_URL || undefined,
+	base: process.env.ASTRO_BASE_PATH || undefined,
+	build: {
+		assetsPrefix: process.env.ASTRO_ASSETS_PREFIX || undefined,
+	},
 	integrations: [
 		mdx(),
 	],
@@ -17,8 +22,12 @@ export default defineConfig({
 	i18n: i18n,
 	env: {
 		schema: {
+			// Deployment configuration
 			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
 			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+			VERSION_TAG: envField.string({ context: 'client', access: 'public', optional: true }),
+			// Application configuration
+			// Add env vars for your application here.
 		},
 		validateSecrets: true,
 	},
